7cbc07d0d058bf5727c249d0d4ead46ef79b9170
2451668 <2451668@students.wits.ac.za>
2024-04-20 15:08:05 +0200

    Defined App

--- src/App.js
+++ src/App.js
@@ -2,3 +2,16 @@
 
 /* disclaimer - as per the brief I'll feature some commentary throughout the build. It might get a little wordy or redundant at times, but I feel for my own sake I'll need the context and annotations - to really understand what it is I'm doing. I've seen the CBO and I think I'll be revisiting this build a lot, it being our first proper submission and all. I understand if this leads to penalties in marking.  */
 
+import React from "react";
+import "./App.css";
+
+function App() {
+  // main app component, defined as a function
+  return (
+    <div className="App">
+      <h1>Local Recipes</h1>
+    </div>
+  );
+}
+
+export default App;
